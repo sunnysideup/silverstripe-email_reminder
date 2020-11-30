cb023ff113d7197d82c61265f4733a28ee0820d2
Shane Watters <shane.watters.nz@gmail.com>
2020-11-30 13:06:30 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/model/EmailReminder_EmailRecord.php
+++ code/model/EmailReminder_EmailRecord.php
@@ -14,6 +14,20 @@ class EmailReminder_EmailRecord extends DataObject
         return self::$plural_name;
     }
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'EmailReminder_EmailRecord';
+
     private static $db = array(
         'EmailTo' => 'Varchar(100)',
         'ExternalRecordClassName' => 'Varchar(100)',
--- code/model/EmailReminder_NotificationSchedule.php
+++ code/model/EmailReminder_NotificationSchedule.php
@@ -60,6 +60,20 @@ class EmailReminder_NotificationSchedule extends DataObject
         return self::$plural_name;
     }
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'EmailReminder_NotificationSchedule';
+
     private static $db = array(
         'DataObject' => 'Varchar(100)',
         'EmailField' => 'Varchar(100)',
